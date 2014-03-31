9994577e786050f6449fd09ee12619eab92595f6
latifs <latifs@latifss-MacBook-Air.local>
2014-03-31 11:33:26 -0700

    added example code from fbgraph and button for login on index

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// added the fbGraph module here
+var graph = require('fbgraph');
+
+// this should really be in a config file!
+// facebook configuration file
+var conf = {
+    client_id:      '176524962546904'
+  , client_secret:  '6e701a88b5a0f15b734e8cdc92abf5b9'
+  , scope:          'email, user_about_me, user_birthday, user_location, publish_stream'
+  , redirect_uri:   'http://localhost:3000/auth/facebook'
+};
 
 var routes = require('./routes');
 var users = require('./routes/user');
@@ -25,6 +36,52 @@ app.use(app.router);
 
 app.get('/', routes.index);
 app.get('/users', users.list);
+
+
+// Fcaebook Routes //////////////////////////
+app.get('/auth/facebook', function(req, res) {
+
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+    var authUrl = graph.getOauthUrl({
+        "client_id":     conf.client_id
+      , "redirect_uri":  conf.redirect_uri
+      , "scope":         conf.scope
+    });
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+    return;
+  }
+
+  // code is set
+  // we'll send that and get the access token
+  graph.authorize({
+      "client_id":      conf.client_id
+    , "redirect_uri":   conf.redirect_uri
+    , "client_secret":  conf.client_secret
+    , "code":           req.query.code
+  }, function (err, facebookRes) {
+    res.redirect('/user');
+  });
+
+
+});
+
+// user gets sent here after being authorized
+app.get('/user', function(req, res) {
+    graph.get("/me/", function(err, res) {
+    console.log(res); // { image: true, location: "http://profile.ak.fb..." }
+});
+  res.render("index", { title: "Logged In" });
+});
+
+///////////////////////////////////////////////////////////////
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
