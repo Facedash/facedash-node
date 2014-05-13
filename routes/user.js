4cb685b35d5d31b1a106df00dda21a8a99788de2
latifs <latifs@latifss-MacBook-Air.local>
2014-05-12 23:41:26 -0700

    cleaning of the main user route

--- routes/user.js
+++ routes/user.js
@@ -6,21 +6,26 @@ var graph = require('fbgraph');
 
 /* GET users listing. */
 exports.info = function(req, res) {
+  
+  var information  = {};
+
   var userLocation = null;
   var userHometown = null;
   var userLocationName = null;
   var userHometownName = null;
+  var friendsCount = null;
+
+  // Percentage function
+  var percentage = function(value1, value2){
+    return Math.floor( ( value1 / ( value2.length ) ) * 100 );
+  }
 
   graph.get('/me/', function(err, data) {
-    console.log('User Data:',data);
+    // console.log('User Data:',data);
     // console.log('User Data:',data.location);
-    userLocation = data.location;
-    userHometown = data.hometown;
-    userHometownName = _.values(userHometown)[1];
-    userLocationName = _.values(userLocation)[1];
+    userHometown = _.values(data.hometown)[1];
+    userLocation = _.values(data.location)[1];
   });
-
-  var friendsCount = null;
 
   // graph.get('/me/friends?fields=id,name,birthday,hometown,location,education,gender', function(err, data) {
   graph.get('/me/friends?fields=id,name,birthday,hometown,location,education,gender,interested_in,relationship_status,timezone,languages', function(err, data) {
@@ -40,48 +45,49 @@ exports.info = function(req, res) {
       return item.gender === 'male' ? 'male': 'female';
     });
 
-    var otherGenderCount = friendsCount - (genderCount.male + genderCount.female);
-    console.log('CustomGender:',otherGenderCount);
+    genderCount.other = friendsCount - (genderCount.male + genderCount.female);
 
-    var otherGenderPerc = Math.floor((otherGenderCount/friendsCount)*100);
-
+    var otherGenderPerc = Math.floor((genderCount.other/friendsCount)*100);
     var malePerc = Math.floor((genderCount.male/friendsCount)*100);
-    var otherGenderPerc = Math.floor((otherGenderCount/friendsCount)*100);
     var femalePerc = 100 - malePerc - otherGenderPerc;
-    // console.log('genderCount;',genderCount);
-    /////////////////////////////////////////////////////////////////////////////
-    
-    var birthdays = _.compact(_.pluck(data.data, 'birthday'));
-    // var validBirthdays = _.compact(birthdays)
-    var trimmedBirthdays = _.reject(birthdays, function(item){
-      return item.length <= 5;
-    });
-
-    var years = _.map(trimmedBirthdays, function(item){
-      return parseInt(item.substr(-4));
-    });
-
-    var sum = _.reduce(years, function(memo, num){ return memo + num; }, 0);
-
-    var d = new Date();
-    var n = d.getFullYear();
-
-    var averageAge = n - Math.floor(sum/years.length);
-    var averageAgeAccuracy = Math.floor((years.length/friendsCount)*100);
-
-    console.log('averageAge:', averageAge);
-    console.log('Accuracy age:', averageAgeAccuracy + '%');
-    
 
     /////////////////////////////////////////////////////////////////////////////
+    
+    // Array of birthday Years
+    var cleanBdays = _.chain(data.data)
+                          .pluck('birthday')
+                          .compact()
+                          .reject(function(item){return item.length <= 5})
+                          .map(function(item){return parseInt(item.substr(-4))})
+                          .value();
 
+    // Sum of array of birthday Years
+    var cleanBdaysAvg = _.chain(cleanBdays)
+                          .reduce(function(memo, num){ return memo + num }, 0)
+                          .value();
 
-    var friendsLocations = _.compact(_.pluck(data.data, 'location'));
+    // console.log('cleanBdays:', cleanBdays);
+    // console.log('cleanBdaysAvg:', cleanBdaysAvg);
+    
+    var year = new Date().getFullYear();
+
+    var averageAge = year - Math.floor(cleanBdaysAvg / cleanBdays.length);
+    var averageAgeAccuracy = Math.floor((cleanBdays.length / friendsCount) * 100);
+
+    // console.log('averageAge:', averageAge);
+    // console.log('Accuracy age:', averageAgeAccuracy + '%');
+    
+
+    /////////////////////////////////////////////////////////////////////////////
+
+    var friendsLocations = _.chain(data.data).pluck('location').compact().value();
     var friendsLocationsCount = friendsLocations.length;
-    var sameLocation = _.where(friendsLocations, userLocation);
+    
+    var sameLocation = _.filter(friendsLocations, function(city){return city['name'] === userLocation});
+    // console.log('sameLocation:',sameLocation);
 
     var sameLocationCount = sameLocation.length;
-    var sameLocationPerc = Math.floor((sameLocationCount/friendsLocationsCount)*100);
+    var sameLocationPerc = percentage(sameLocationCount,friendsLocations);
     var sameLocationAccuracy = Math.floor((friendsLocationsCount/friendsCount)*100);
 
     // console.log(sameLocation);
@@ -90,12 +96,14 @@ exports.info = function(req, res) {
 
     /////////////////////////////////////////////////////////////////////////////
 
-    var friendsHometowns = _.compact(_.pluck(data.data, 'hometown'));
+    var friendsHometowns = _.chain(data.data).pluck('hometown').compact().value();
     var friendsHometownsCount = friendsHometowns.length;
-    var sameHometown = _.where(friendsHometowns, userHometown);
+
+    var sameHometown = _.filter(friendsHometowns, function(city){return city['name'] === userHometown});
+    // var sameHometown = _.where(friendsHometowns, userHometown);
 
     var sameHometownCount = sameHometown.length;
-    var sameHometownPerc = Math.floor((sameHometownCount/friendsHometownsCount)*100);
+    var sameHometownPerc = percentage(sameHometownCount,friendsHometowns);
     var sameHometownAccuracy = Math.floor((friendsHometownsCount/friendsCount)*100);
 
     console.log('same Hometown Perc:',sameHometownPerc);
@@ -104,103 +112,49 @@ exports.info = function(req, res) {
 
     /////////////////////////////////////////////////////////////////////////////
     var friendsRstatus = _.compact(_.pluck(data.data, 'relationship_status'));
-    
-    // console.log('friendsRStatus Count:',friendsRstatus.length);
 
-    var status = {};
+    // Count per relationship status
+    var relationshipStatus = {};
     var friendsRstatusCount = _.each(friendsRstatus, function(item){
-      (!status[item]) ? status[item] = 1 : status[item]++;
+      (!relationshipStatus[item]) ? relationshipStatus[item] = [1] : relationshipStatus[item][0]++;
     });
-    // console.log(status);
 
-    var single = (status['Single']) ? status['Single'] : 0;
-    var inArelationship = (status['In a relationship']) ? status['In a relationship'] : 0;
-    var engaged = (status['Engaged']) ? status['Engaged'] : 0;
-    var married = (status['Married']) ? status['Married'] : 0;
-    var civilUnion = (status['In a civil union']) ? status['In a civil union'] : 0;
-    var domesticPartnership = (status['In a domestic partnership']) ? status['In a domestic partnership'] : 0;
-    var openRelationship = (status['In an open relationship']) ? status['In an open relationship'] : 0;
-    var complicated = (status['it\'s complicated']) ? status['it\'s complicated'] : 0;
-    var separated = (status['Separated']) ? status['Separated'] : 0;
-    var divorced = (status['Divorced']) ? status['Divorced'] : 0;
-    var widowed = (status['Widowed']) ? status['Widowed'] : 0;
-    
+    // Percentage per relationship status
+    _.each(relationshipStatus, function(value, key){
+      relationshipStatus[key].push(percentage(value, friendsRstatus));
+    });
 
-    var singlePerc = Math.floor((single/(friendsRstatus.length))*100);
-    var inArelationshipPerc = Math.floor((inArelationship/(friendsRstatus.length))*100);
-    var engagedPerc = Math.floor((engaged/(friendsRstatus.length))*100);
-    var marriedPerc = Math.floor((married/(friendsRstatus.length))*100);
-    var civilUnionPerc = Math.floor((civilUnion/(friendsRstatus.length))*100);
-    var domesticPartnershipPerc = Math.floor((domesticPartnership/(friendsRstatus.length))*100);
-    var openRelationshipPerc = Math.floor((openRelationship/(friendsRstatus.length))*100);
-    var complicatedPerc = Math.floor((complicated/(friendsRstatus.length))*100);
-    var separatedPerc = Math.floor((separated/(friendsRstatus.length))*100);
-    var divorcedPerc = Math.floor((divorced/(friendsRstatus.length))*100);
-    var widowedPerc = Math.floor((widowed/(friendsRstatus.length))*100);
-
+    console.log(relationshipStatus);
     
     var friendsRstatusCount = friendsRstatus.length;
     var friendsRstatusAccuracy = Math.floor((friendsRstatusCount/friendsCount) * 100);
-    console.log('friendsCount: ',friendsCount);
-    console.log('friendsRstatusCount: ',friendsRstatusCount);
-    console.log('friendsRstatusAccuracy: ',friendsRstatusAccuracy);
-
-    // console.log('SingleFriends:',singlePerc);
-    // console.log('In a relationship:',inArelationshipPerc);
-    // console.log(domesticPartnership);
-
-
-
-    // console.log(sameHometown);
-    // console.log('Num sameHometown:',sameHometown.length);
-
-    // console.log(locationNames);
-
-    // console.log('User Location:',userLocation);
-    // console.log('User hometown:',userHometown);
+    // console.log('friendsCount: ',friendsCount);
+    // console.log('friendsRstatusCount: ',friendsRstatusCount);
+    // console.log('friendsRstatusAccuracy: ',friendsRstatusAccuracy);
     
     // res.render('user', { friendsCount: friendsCount, userLocationName: userLocationName, userHometownName: userHometownName, otherGenderCount: otherGenderCount, otherGenderPerc: otherGenderPerc, maleCount: genderCount.male, malePerc: malePerc, femaleCount: genderCount.female, femalePerc: femalePerc, averageAge: averageAge, averageAgeAccuracy: averageAgeAccuracy, sameLocationCount: sameLocationCount,sameLocationPerc: sameLocationPerc, sameLocationAccuracy: sameLocationAccuracy, sameHometownCount: sameHometownCount,sameHometownPerc: sameHometownPerc, sameHometownAccuracy: sameHometownAccuracy, single: single, inArelationship: inArelationship, engaged: engaged, married: married, civilUnion: civilUnion, domesticPartnership: domesticPartnership, openRelationship: openRelationship, complicated: complicated, separated: separated, divorced: divorced, widowed: widowed, singlePerc: singlePerc, inArelationshipPerc: inArelationshipPerc, engagedPerc: engagedPerc, marriedPerc: marriedPerc, civilUnionPerc: civilUnionPerc, domesticPartnershipPerc: domesticPartnershipPerc, openRelationshipPerc: openRelationshipPerc, complicatedPerc: complicatedPerc, separatedPerc: separatedPerc, divorcedPerc: divorcedPerc, widowedPerc: widowedPerc, friendsRstatusAccuracy: friendsRstatusAccuracy});
     res.send({  information: {
                   friendsNumber: friendsCount,
-                  location: userLocationName,
-                  hometown: userHometownName,
+                  location: userLocation,
+                  hometown: userHometown,
                   averageAge: averageAge
                 },
+                
+                relationshipStatus : relationshipStatus,
+
                 counts: {
-                  otherGender: otherGenderCount,
+                  otherGender: genderCount.other,
                   male: genderCount.male,
                   female: genderCount.female,
                   sameLocation: sameLocationCount,
-                  sameHometown: sameHometownCount,
-                  single: single,
-                  inArelationship: inArelationship,
-                  engaged: engaged,
-                  married: married,
-                  civilUnion: civilUnion,
-                  domesticPartnership: domesticPartnership,
-                  openRelationship: openRelationship,
-                  complicated: complicated,
-                  separated: separated,
-                  divorced: divorced,
-                  widowed: widowed
+                  sameHometown: sameHometownCount
                 },
                 percentage: {
                   otherGender: otherGenderPerc,
                   male: malePerc,
                   female: femalePerc,
                   sameLocation: sameLocationPerc,
-                  sameHometown: sameHometownPerc,
-                  single: singlePerc,
-                  inArelationship: inArelationshipPerc,
-                  engaged: engagedPerc,
-                  married: marriedPerc,
-                  civilUnion: civilUnionPerc,
-                  domesticPartnership: domesticPartnershipPerc,
-                  openRelationship: openRelationshipPerc,
-                  complicated: complicatedPerc,
-                  separated: separatedPerc,
-                  divorced: divorcedPerc,
-                  widowed: widowedPerc
+                  sameHometown: sameHometownPerc
                 },
                 accuracy:{
                   averageAge: averageAgeAccuracy,
